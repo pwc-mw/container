224872541b32106ed3c44662febef6efb45a0179
michelwu <michel.wu@pwc.com>
2023-04-14 09:34:02 +0200

    first commit

---
+++ src/wwElement.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <slot class="my-element"> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    textStyle() {
+      return {
+        color: this.content.textColor,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  position: relative;
+  height: inherit;
+  color: inherit;
+  background-color: inherit;
+}
+</style>
